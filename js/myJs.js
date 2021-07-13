1062aa703ca2817244732274653b474e76ae3e69
Ghvgjbhjjbgh1246y <87361088+Ghvgjbhjjbgh1246y@users.noreply.github.com>
2021-07-13 15:37:22 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "He luu!",
+  text2: "A có điều này muốn hỏi e nhớ phải trả lời thật lòng nhaaa.",
+  text3: "E có yêu a thật lòng không ._.",
+  text4: "Nếu e mà nói dối là tui dỗi nha :v",
+  text5: " liu liu e đùa đó ",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text7: "lí do em thích anh đi :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Vì a dễ thưn vlllll",
+  text10: "A biết mà ^^ Yêu e ❤",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Tối nay call nhá  :v"
 };
 
 $(document).ready(function () {
